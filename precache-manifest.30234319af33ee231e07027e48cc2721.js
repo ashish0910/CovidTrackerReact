c2605f6d3edcf637add35669b25f6419e09c2c97
ashish0910 <ashish09101998@gmail.com>
2020-10-09 11:58:41 +0530

    Updates

---
+++ precache-manifest.30234319af33ee231e07027e48cc2721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96493a930df78d5573946f8c79fd6fd8",
+    "url": "/CovidTrackerReact/index.html"
+  },
+  {
+    "revision": "c2b2b9e7d2f39b932afc",
+    "url": "/CovidTrackerReact/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "dab05dcd54b486d493a2",
+    "url": "/CovidTrackerReact/static/css/main.1a9bd970.chunk.css"
+  },
+  {
+    "revision": "c2b2b9e7d2f39b932afc",
+    "url": "/CovidTrackerReact/static/js/2.f9a0f088.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/CovidTrackerReact/static/js/2.f9a0f088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab05dcd54b486d493a2",
+    "url": "/CovidTrackerReact/static/js/main.b6ff9f1b.chunk.js"
+  },
+  {
+    "revision": "aba1258d2d1e789793d7",
+    "url": "/CovidTrackerReact/static/js/runtime-main.a0fed6e7.js"
+  }
+]);
